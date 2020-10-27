c88b73ed9a00621ba0c699fc67b2cd64a91b486d
Terry <weightang@gmail.com>
2020-10-27 22:25:05 +0800

    创建价格列表

--- lib/models/viewModel/pirce_model.dart
+++ lib/models/viewModel/pirce_model.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_app/api/app_database.dart';
 import 'package:flutter_app/models/response/price_entity.dart';
 import 'package:flutter_app/provider/view_state_model.dart';
+import 'package:flutter_app/utils/platform_utils.dart';
 
 class PriceModel extends ViewStateModel {
 
@@ -18,7 +19,23 @@ class PriceModel extends ViewStateModel {
   }
 
   Future<List<PriceEntity>> findAllPrice() async{
-    return DataBase.basisDatabase.basisDao.findAllPrice();
+    try{
+     var list=  await DataBase.basisDatabase.basisDao.findAllPrice();
+      if(list.isEmpty){
+        setEmpty();
+        return list;
+      }else{
+        setIdle();
+        return list;
+     }
+    }catch(e,s){
+      setError(e, s);
+      return null;
+    }
+
+
+
+
   }
 
 
--- lib/ui/page/setting/price_page.dart
+++ lib/ui/page/setting/price_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/response/price_entity.dart';
 import 'package:flutter_app/models/viewModel/pirce_model.dart';
 import 'package:flutter_app/provider/provider_widget.dart';
+import 'package:flutter_app/provider/view_state_widget.dart';
 
 
 class PricePage extends StatefulWidget {
@@ -13,7 +15,7 @@ class _PricePageState extends State<PricePage> {
   Widget build(BuildContext context) {
     return ProviderWidget<PriceModel>(
       model: PriceModel(),
-      builder: (context,model,child){
+      builder: (context, model, child) {
         return Scaffold(
           appBar: AppBar(
             centerTitle: true,
@@ -29,7 +31,7 @@ class _PricePageState extends State<PricePage> {
             child: Column(
               children: <Widget>[
                 PriceListView(model: model),
-                PriceTextField(model:model),
+                PriceTextField(model: model),
 
               ],
 
@@ -42,20 +44,43 @@ class _PricePageState extends State<PricePage> {
     );
   }
 }
+
 class PriceListView extends StatelessWidget {
- final PriceModel model;
+  final PriceModel model;
 
   PriceListView({@required this.model});
 
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ProviderWidget<PriceModel>(
+        model: model,
+        builder: (context, model, child) {
+          if (model.isEmpty) {
+            return Container();
+          } else if (model.isError) {
+            return ViewStateErrorWidget(error: model.viewStateError);
+          }
+          List<PriceEntity> priceList;
+          model.findAllPrice().then((value) => priceList=value);
+         return ListView.builder(
+           itemCount: priceList.length,
+           itemBuilder: (_,index){
+
+             return Container();
+
+           },
+
+         );
+
+        }
+    );
   }
 }
 
 class PriceTextField extends StatelessWidget {
   final PriceModel model;
+
   PriceTextField({@required this.model});
 
   @override
